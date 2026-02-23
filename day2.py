f8e08612cd71596c1f7d17c82d38b0af8da94b11
MowahiburRahmanTwake <twake.edu@gmail.com>
2026-02-23 22:33:26 +0600

    day 2 complet

--- day2.py
+++ day2.py
@@ -20,10 +20,34 @@
 #     print("Grade: F")
     
 # Positive / Negative / Zero CheckerPython
-num = float(input("Enter a number: "))
-if num > 0:
-    print("Positive")
-elif num < 0:
-    print("Negative")
-else:
-    print("Zero")
+# num = float(input("Enter a number: "))
+# if num > 0:
+#     print("Positive")
+# elif num < 0:
+#     print("Negative")
+# else:
+#     print("Zero")
+# 1 to 100 Print (for loop + while loop দুটোই করো)
+# for loop
+# for i in range(1, 101):
+#     print(i, end=" ")
+# print()  # for new line
+# while loop
+# i = 1
+# while i <= 100:
+#     print(i, end= " ")
+#     i += 1
+
+# Multiplication Table (user-এর number-এর 1-10 table)
+correct_pass = "secret123"
+attempts = 3
+while attempts > 0:
+    user_pass = input(f"Enter password ({attempts} attempts left): ")
+    if user_pass == correct_pass:
+        print("Access granted!")
+        break
+    else:
+        print("Wrong password!")
+        attempts -= 1
+if attempts == 0:
+    print("Access denied. Too many wrong attempts.")
