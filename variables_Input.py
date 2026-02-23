5a4c2d46248e1a091fe867148b698b5df106bb02
MowahiburRahmanTwake <twake.edu@gmail.com>
2026-02-23 19:13:28 +0600

    Variables + Input

---
+++ variables_Input.py
@@ -0,0 +1,5 @@
+#input example
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hi {name}, you are {age} yers old.")
+print(f"In one year, you will be {age + 1} years old.")
