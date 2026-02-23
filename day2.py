35b99e2110aac6ac6fca58d84647b0a79d875d8a
MowahiburRahmanTwake <twake.edu@gmail.com>
2026-02-23 21:30:51 +0600

    Positive / Negative / Zero CheckerPython

--- day2.py
+++ day2.py
@@ -7,16 +7,23 @@
 
     # Grade Calculator (if-elif-else)
 
-marks = int(input("Enter your marks: "))
-if marks >= 80:
-    print("Grade: A+")
-elif marks >= 70:
-    print("Grade: B")
-elif marks >= 60:
-    print("Grade: C")
-elif marks >= 50:
-    print("Grade: D")
-else:
-    print("Grade: F")
+# marks = int(input("Enter your marks: "))
+# if marks >= 80:
+#     print("Grade: A+")
+# elif marks >= 70:
+#     print("Grade: B")
+# elif marks >= 60:
+#     print("Grade: C")
+# elif marks >= 50:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
     
-
+# Positive / Negative / Zero CheckerPython
+num = float(input("Enter a number: "))
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
